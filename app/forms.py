1fcca38c03ccccdffeca6e4484395bce5733dffd
a2b123 <atbhati2@gmail.com>
2017-11-21 23:49:06 -0600

    Deploy

---
+++ app/forms.py
@@ -0,0 +1,22 @@
+from django import forms 
+
+from django.contrib.auth.models import User
+from app.models import Restaurant
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+	email = forms.CharField(max_length=100, required=True)
+	password = forms.CharField(widget=forms.PasswordInput())
+	
+	class Meta:
+		model = User
+		fields = ("username", "password", "first_name", "last_name", "email")
+
+
+class RestaurantForm(forms.ModelForm):
+	class Meta:
+		model = Restaurant
+		fields = ("name", "phone", "address", "logo")
+
